442cb6e2cb4e14c0451fbaccbabc1b45bc02bfda
Humayun1318 <humayunKabir6267@gmail.com>
2025-01-07 23:47:33 +0600

    navbar done

---
+++ js/utilities.js
@@ -0,0 +1,20 @@
+
+
+//Menubar click function
+const menuBar = document.getElementById('menu-bar')
+const menuList = document.getElementById('menu-list');
+const crossBar = document.getElementById('cross-bar');
+function forMenuBar() {
+  menuList.classList.remove("hidden");
+  menuList.classList.add('absolute', "top-12", 'right-8');
+  crossBar.classList.remove('hidden')
+  menuBar.classList.add('hidden')
+
+}
+
+function forCrossBar() {
+  
+  menuList.classList.add('hidden');
+  menuBar.classList.remove('hidden')
+  crossBar.classList.add('hidden')
+}
